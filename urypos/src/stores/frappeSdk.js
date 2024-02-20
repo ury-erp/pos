3221c18d5ae5c451046dc6fe49793d0bffede64f
Sanjusha_tridz <sanjusha@tridz.com>
2024-02-20 05:01:57 +0000

    Fix:Update URL construction to include port properly

--- urypos/src/stores/frappeSdk.js
+++ urypos/src/stores/frappeSdk.js
@@ -3,7 +3,7 @@ import { FrappeApp } from "frappe-js-sdk";
 let host = window.location.hostname;
 let port = window.location.port;
 let protocol = port ? "http" : "https";
-let url = `${protocol}://${host}${port}`;
+let url = `${protocol}://${host}:${port}`;
 
 export const frappe = new FrappeApp(url);
 
