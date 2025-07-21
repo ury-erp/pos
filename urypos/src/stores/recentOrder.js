237d0d48eca9933d1deff1c655d50e3a5fc93d74
Sanjusha_tridz <sanjusha@tridz.com>
2025-07-21 06:38:39 +0000

    Fix:Payment before print

--- urypos/src/stores/recentOrder.js
+++ urypos/src/stores/recentOrder.js
@@ -428,6 +428,7 @@ export const usetoggleRecentOrder = defineStore("recentOrders", {
               "OK"
             );
             this.isLoading = false;
+            this.showPayment = false;
           } else {
             this.showPayment = true;
           }
