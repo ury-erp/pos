e1a6d81a2aa5bed937b2c8026d5d632e8a4bbc9e
swafa-as <swafa@tridz.com>
2025-08-28 06:23:02 +0000

    fix:return success and update retry logic for multi-cashier printing

--- urypos/src/stores/invoiceData.js
+++ urypos/src/stores/invoiceData.js
@@ -559,13 +559,11 @@ export const useInvoiceDataStore = defineStore("invoiceData", {
                 const sendObj = {
                   invoice: invoiceNo,
                 };
-                await this.call
-                  .post("ury.ury.api.ury_print.qz_print_update", sendObj)
-                  .then(() => {
+                  await this.call.post("ury.ury.api.ury_print.qz_print_update", sendObj)
+                  setTimeout(() => {
                     window.location.reload();
-                    return 200;
-                  });
-                  return;
+                  }, 1000);
+                  return "Success"
               }
               errorMessage = res;
               i++;
